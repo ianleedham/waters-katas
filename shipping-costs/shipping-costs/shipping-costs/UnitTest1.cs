55f09da36dd3a0a4787087d5a75da74e908ae1bd
Ian-Leedham_waters <Ian_Leedham@waters.com>
2022-11-15 15:32:34 +0000

    Adding extra logic for first class, refactoring and a test for next day delivery

--- shipping-costs/shipping-costs/shipping-costs/UnitTest1.cs
+++ shipping-costs/shipping-costs/shipping-costs/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace shipping_costs
     
     // We only deliver within the UK, which includes Northern Ireland, the Channel Islands, the Isle of Man and the Scottish islands.
     // Regular postage costs £4.99 per item and is free for orders totalling over £25 where there are no exceptional items (see below).
-    // First class delivery in the UK: add £2.99 per item.
+    // First class delivery in the UK: add £2.99 per item on top of regular postage costs.
     // Next day delivery within the UK: add £11.99 per item to all orders. (This option is not available off the mainland.)
     // Large items (any dimension over 30cm): add £19.90 per item; first class is the only available option.
     // Heavy items (over 15kg): add £39:90 each item, again first class only, and only available in mainland UK.
@@ -34,6 +34,18 @@ namespace shipping_costs
         }
 
         [TestMethod]
+        public void PostageForOneItem_UnderThreshold_WithNextDayDelivery()
+        {
+            var calculator = new PostageCalculator();
+            var totalOrderCost = 15.25;
+            var expectedPostageCost = 11.99 + 4.99;
+
+            var postageCost = calculator.CalculatePostage("nextdaydelivery", totalOrderCost, 1);
+
+            Assert.AreEqual(expectedPostageCost, postageCost);
+        }
+
+        [TestMethod]
         public void RegularPostageForTwoItemsUnderFreeThreshold()
         {
             var calculator = new PostageCalculator();
@@ -46,15 +58,29 @@ namespace shipping_costs
         }
 
         [TestMethod]
-        public void FirstClassPostageForOneItemOverFreeThreshold()
+        [DataRow(23.01, 7.98, DisplayName = "FirstClass Postage For One Item Under Free Threshold")]
+        [DataRow(25.01, 2.99, DisplayName = "FirstClass Postage For One Item Over Free Threshold")]
+        public void FirstClassPostageForOneItem_FreeThresholds(double totalOrderCost, double expectedPostageCost)
         {
             var calculator = new PostageCalculator();
-            var totalOrderCost = 25.01;
             var nItems = 1;
 
             var postageCost = calculator.CalculatePostage("first", totalOrderCost, nItems);
 
-            Assert.AreEqual(2.99, postageCost);
+            Assert.AreEqual(expectedPostageCost, postageCost);
+        }
+
+        [TestMethod]
+        [DataRow(23.01, 15.96, DisplayName = "FirstClass Postage For Two Items Under Free Threshold")]
+        [DataRow(25.01, 5.98, DisplayName = "FirstClass Postage For Two Items Over Free Threshold")]
+        public void FirstClassPostageForTwoItems_FreeThresholds(double totalOrderCost, double expectedPostageCost)
+        {
+            var calculator = new PostageCalculator();
+            var nItems = 2;
+
+            var postageCost = calculator.CalculatePostage("first", totalOrderCost, nItems);
+
+            Assert.AreEqual(expectedPostageCost, postageCost);
         }
     }
 
@@ -62,18 +88,35 @@ namespace shipping_costs
     {
         private const double FreePostage = 0;
         private const double StandardPostagePerItem= 4.99;
+        private const double FirstClassPostagePerItem = 2.99;
+        private const string FirstClassPostageName = "first";
 
         private const double FreePostageThreshold = 25;
 
         public double CalculatePostage(string deliveryClass, double totalOrderCost, int nItems)
         {
-            if (totalOrderCost > FreePostageThreshold)
+            if (deliveryClass == FirstClassPostageName)
             {
-                return FreePostage;
+                return CalculateFirstClassPostage(totalOrderCost, nItems);
             }
 
-            return nItems * StandardPostagePerItem;
-
+            return CalculateStandardPostage(nItems, totalOrderCost);
         }
+
+        private double CalculateFirstClassPostage(double totalOrderCost, int nItems)
+        {
+            return CalculateFirstClassSupplement(nItems) + CalculateStandardPostage(nItems, totalOrderCost);
+        }
+
+        private double CalculateFirstClassSupplement(int nItems) =>  nItems * FirstClassPostagePerItem;
+
+        private double CalculateStandardPostage(int nItems, double totalOrderCost)
+        {
+            if(IsUnderFreeThreshold(totalOrderCost))
+                return nItems * StandardPostagePerItem;
+            return FreePostage;
+        }
+
+        private bool IsUnderFreeThreshold(double totalOrderCost) => totalOrderCost <= FreePostageThreshold;
     }
 }
